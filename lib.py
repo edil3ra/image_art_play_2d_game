5e950ad162da46346b7a1a18df2d5002d8dc619a
edil3ra <edil3ra@gmail.com>
2022-02-12 11:55:30 +0100

    Adatp to new api, use machine learning to guess the colors

--- lib.py
+++ lib.py
@@ -34,35 +34,42 @@ def plot(subplots=[], save_as=None, fig_h=9):
 def pixelate_images_in_folder(
     in_dir,
     out_dir,
-    image_model,
     palette,
+    image_model=None,
     factor=None,
     width=None,
     height=None,
     dither="none",
-    boost=False,
 ):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-
-    model = Pyx(
-        factor=factor,
-        width=width,
-        height=height,
-        palette=palette,
-        dither=dither,
-        boost=boost,
-    ).fit(image_model)
+    model = None
+    if image_model is not None:
+        model = Pyx(
+            factor=factor,
+            width=width,
+            height=height,
+            palette=palette,
+            dither=dither,
+        ).fit(image_model)
 
     for filename in os.listdir(in_dir):
         in_path = in_dir + "/" + filename
         out_path = out_dir + "/" + filename
         image = io.imread(in_path)
+        if image_model is None:
+            model = Pyx(
+                factor=factor,
+                width=width,
+                height=height,
+                palette=palette,
+                dither=dither,
+            ).fit(image)
         transformed_image = model.transform(image)
         io.imsave(out_path, transformed_image)
 
 
-def generate_assets(from_dir, to_dir, palette_image, palette_count, items, dither="naive", boost=False):
+def generate_assets(from_dir, to_dir, palette_count, items, palette_image=None, dither="naive"):
     if not os.path.exists(to_dir):
         os.makedirs(to_dir)
     
@@ -72,59 +79,11 @@ def generate_assets(from_dir, to_dir, palette_image, palette_count, items, dithe
         pixelate_images_in_folder(
             from_subdir,
             to_subdir,
-            image_model=io.imread(palette_image),
+            image_model=io.imread(palette_image) if palette_image is not None else None,
             palette=palette_count,
             width=item.get('width'),
             height=item.get('height'),
             dither=dither,
-            boost=boost,
         )
 
 
-    
-
-        
-# def generate_shooter_game():
-#     from_main_dir = "images/normal"
-#     to_main_dir = "images/pixelate"
-#     palette_image = io.imread("images/palettes/mulfok32-32x.png")
-#     palette = 16
-#     boost = True
-#     subdirs = [
-#         ("special_units", 64, 64),
-#         ("overloadMonster", 64, 64),
-#         ("top_down_spaceships", 64, 64),
-#         ("simple_explosion", 64, 64),
-#         ("sonic_explosion", 64, 64),
-#         ("filled", None, None),
-#         ("laser_beams", None, None),
-#         ("bullets", None, None),
-#         ("spaceBackground", None, None),
-#         ("stars", None, None),
-#         ("main_weapons", None, None),
-#         ("beam_jet", None, None),
-#     ]
-
-#     for (dir, width, height) in tqdm(subdirs):
-#         from_dir = f'{from_main_dir}/{dir}'
-#         to_dir = f'{to_main_dir}/{dir}'
-        
-#         pixelate_images_in_folder(
-#             from_dir,
-#             to_dir,
-#             palette_image,
-#             palette=palette,
-#             width=width,
-#             height=height,
-#             dither="naive",
-#             boost=boost,
-#         )
-
-
-
-# def main():
-#     generate_shooter_game()
-
-
-# if __name__ == '__main__':
-#     main()
